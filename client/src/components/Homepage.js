4870f5bc1bca85be58c230b24d08b24336448efc
Tim Meehan <meehanti23@gmail.com>
2023-12-07 12:37:34 -0500

    Create route to homepage

---
+++ client/src/components/Homepage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Homepage = (props) => {
+    return (
+        <>
+            <h1>Tim and Abbie's Wedding!</h1>
+        </>
+    )
+}
+
+export default Homepage;
